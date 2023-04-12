6f17ffe72148df92a634fd76e9aa0d89166ec136
Miguel Andrés Lizarzado Zamora <124523459+Miguelizarzado@users.noreply.github.com>
2023-04-12 18:27:58 +0000

    Modificación 12/04/23

--- script.js
+++ script.js
@@ -1,48 +1,60 @@
+function verificarSuma () {
+    let resultadoSuma = parseFloat(primerValor.value) + parseFloat(segundoValor.value);
+    document.getElementById("resultado").innerHTML = resultadoSuma;
+    
+    if (resultadoSuma % 1 != 0) {
+        document.getElementById("resultado").innerHTML = resultadoSuma.toFixed(4);
+    }
+}
+
+function verificarResta () {
+    var resultadoResta = parseFloat(primerValor.value) - parseFloat(segundoValor.value);
+    document.getElementById("resultado").innerHTML = resultadoResta;
+
+    if (resultadoResta % 1 != 0) {
+        document.getElementById("resultado").innerHTML = resultadoResta.toFixed(4);
+    }
+}
+
+function verificarMultiplicar () {
+    var resultadoMultiplicar = parseFloat(primerValor.value) * parseFloat(segundoValor.value);
+    document.getElementById("resultado").innerHTML = resultadoMultiplicar;
+
+    if (resultadoMultiplicar % 1 != 0) {
+        document.getElementById("resultado").innerHTML = resultadoMultiplicar.toFixed(4);
+    }
+}
+
+function verificarDividir () {
+    var resultadoDividir = parseFloat(primerValor.value) / parseFloat(segundoValor.value);
+    document.getElementById("resultado").innerHTML = resultadoDividir;
+
+    if (resultadoDividir % 1 != 0) {
+        document.getElementById("resultado").innerHTML = resultadoDividir.toFixed(4);
+    }
+}
+
+function clear () {
+
+    document.getElementById("primerValor").value = "";
+    document.getElementById("segundoValor").value = "";
+    document.getElementById("resultado").innerHTML = "";
+    primerValor.focus();
+
+}
+
 let primerValor = document.getElementById("primerValor");
-        let segundoValor = document.getElementById("segundoValor");
+let segundoValor = document.getElementById("segundoValor");
+let botonSuma = document.getElementById("botonSuma");
+let botonResta = document.getElementById("botonResta");
+let botonMultiplicar = document.getElementById("botonMultiplicar");
+let botonDividir = document.getElementById("botonDividir");
+let botonClear = document.getElementById("botonClear");
 
+primerValor.focus();
 
-        let botonSuma = document.getElementById("botonSuma");
-
-        function verificarSuma () {
-            let resultadoSuma = parseFloat(primerValor.value) + parseFloat(segundoValor.value);
-            document.getElementById("resultado").innerHTML = resultadoSuma.toFixed(6);
-        }
-        botonSuma.onclick = verificarSuma;
-
-        let botonResta = document.getElementById("botonResta");
-        
-        function verificarResta () {
-            var resultadoResta = parseFloat(primerValor.value) - parseFloat(segundoValor.value);
-            document.getElementById("resultado").innerHTML = resultadoResta.toFixed(6);
-        }
-        botonResta.onclick = verificarResta;
-
-
-        let botonMultiplicar = document.getElementById("botonMultiplicar");
-
-        function verificarMultiplicar () {
-            var resultadoMultiplicar = parseFloat(primerValor.value) * parseFloat(segundoValor.value);
-            document.getElementById("resultado").innerHTML = resultadoMultiplicar.toFixed(6);
-        }
-        botonMultiplicar.onclick = verificarMultiplicar;
-
-
-        let botonDividir = document.getElementById("botonDividir");
-        
-        function verificarDividir () {
-            var resultadoDividir = parseFloat(primerValor.value) / parseFloat(segundoValor.value);
-            document.getElementById("resultado").innerHTML = resultadoDividir.toFixed(6);
-        }
-        botonDividir.onclick = verificarDividir;
-
-        let botonClear = document.getElementById("botonClear");
-
-        function clear () {
-
-            document.getElementById("primerValor").value = "";
-            document.getElementById("segundoValor").value = "";
-            document.getElementById("resultado").innerHTML = "";
-
-        }
-        botonClear.onclick = clear;
+botonSuma.onclick = verificarSuma;
+botonResta.onclick = verificarResta;
+botonMultiplicar.onclick = verificarMultiplicar;
+botonDividir.onclick = verificarDividir;
+botonClear.onclick = clear;
